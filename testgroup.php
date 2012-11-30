952c8188a596c341efeaa15da2381829497c0c1e
Helmut Januschka <helmut@januschka.com>
2012-11-30 11:35:26 +0100

    added shm_place to map calls

--- testgroup.php
+++ testgroup.php
@@ -1,6 +1,7 @@
 <?
 $cfg = "/storage/SF.NET/BARTLBY/GIT/bartlby-core/bartlby.cfg";
 
-bartlby_modify_servicegroup($cfg, "Name neu", 0, 0, "|1|2|3", 4);
-bartlby_toggle_servicegroup_notify($cfg, 1, 1);
+var_dump(bartlby_servergroup_map($cfg));
+
+bartlby_delete_servergroup($cfg, 1);
 ?>
