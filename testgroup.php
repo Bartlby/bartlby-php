96be2004dd910c0873f4d511681e075d737953c0
hjanuschka <helmut@januschka.com>
2012-11-30 11:20:47 +0100

    php frontend functions for server/service groups

---
+++ testgroup.php
@@ -0,0 +1,6 @@
+<?
+$cfg = "/storage/SF.NET/BARTLBY/GIT/bartlby-core/bartlby.cfg";
+
+bartlby_modify_servicegroup($cfg, "Name neu", 0, 0, "|1|2|3", 4);
+bartlby_toggle_servicegroup_notify($cfg, 1, 1);
+?>
