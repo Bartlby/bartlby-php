c16ae294b47bc90b0c4bff76761a928c01240b5a
hjanuschka <helmut@januschka.com>
2012-12-01 20:42:46 +0100

    a

--- testgroup.php
+++ testgroup.php
@@ -2,6 +2,6 @@
 $cfg = "/storage/SF.NET/BARTLBY/GIT/bartlby-core/bartlby.cfg";
 $map = bartlby_svc_map($cfg, null, null);
 
+bartlby_modify_servergroup($cfg, "asdf", 1,1,"asd", 3);
 
-var_dump($map);
 ?>
