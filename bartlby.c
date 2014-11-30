a48bda7d6560f9f354cd4c42d4e2810a29da8d8b
Helmut Januschka <helmut@januschka.com>
2014-12-01 00:52:36 +0100

    intermediate files for traps

--- bartlby.c
+++ bartlby.c
@@ -171,6 +171,13 @@ zend_function_entry bartlby_functions[] = {
 	
 	
 	PHP_FE(bartlby_notification_log_at_index, NULL)
+
+	PHP_FE(bartlby_get_trap, NULL)
+	PHP_FE(bartlby_add_trap, NULL)
+	PHP_FE(bartlby_modify_trap, NULL)
+	PHP_FE(bartlby_delete_trap, NULL)
+	PHP_FE(bartlby_set_trap_id, NULL)
+
 	
 	{NULL, NULL, NULL}	/* Must be the last line in bartlby_functions[] */
 };
@@ -480,6 +487,7 @@ int bartlby_mark_object_gone(zval * zbartlby_resource, bartlby_res * bres, long 
 	struct btl_event * evntmap;
 	struct servergroup * srvgrpmap;
 	struct servicegroup * svcgrpmap;
+	struct trap * trapmap;
 	char * tmpstr;
 	
 	int audit_type=0;
@@ -495,6 +503,7 @@ int bartlby_mark_object_gone(zval * zbartlby_resource, bartlby_res * bres, long 
 	evntmap=(struct btl_event *)(void *)&srvmap[shm_hdr->srvcount+1];
 	srvgrpmap=(struct servergroup *)(void *)&evntmap[EVENT_QUEUE_MAX+1];
 	svcgrpmap=(struct servicegroup *)(void *)&srvgrpmap[shm_hdr->srvgroupcount+1];
+	trapmap=(struct trap *)(void *)&svcgrpmap[shm_hdr->svcgroupcount+1];
 	
 
 
@@ -549,6 +558,14 @@ int bartlby_mark_object_gone(zval * zbartlby_resource, bartlby_res * bres, long 
 					}	
 				}	
 				audit_type=BARTLBY_AUDIT_TYPE_SERVERGROUP;
+			break;
+			case BARTLBY_TRAP_GONE:
+				for(x=0; x<shm_hdr->trapcount; x++) {
+					if(trapmap[x].trap_id == id) {
+						//srvgrpmap[x].is_gone = msg;
+					}	
+				}	
+				audit_type=BARTLBY_AUDIT_TYPE_TRAP;
 			break;
 			
 				
@@ -5197,5 +5214,376 @@ PHP_FUNCTION(bartlby_toggle_servicegroup_active) {
 		
 }
 
+
+///TRAP
+PHP_FUNCTION(bartlby_get_trap) {
+
+	struct shm_header * shm_hdr;
+	struct service * svcmap;
+	struct worker * wrkmap;
+	struct downtime * dtmap;
+	struct btl_event * evntmap;
+	struct server * srvmap;	
+	struct servergroup * srvgrpmap;
+	struct servicegroup * svcgrpmap;
+	struct trap * trapmap;
+	int is_down;
+	int current_time;
+	int dtmapindex;
+
+
+	int y, z, u;
+	zval * zbartlby_resource;
+	zval * bartlby_trap_id;
+	bartlby_res * bres;
+	
+	if (ZEND_NUM_ARGS() != 2 || zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rz", &zbartlby_resource, &bartlby_trap_id)==FAILURE) {
+		WRONG_PARAM_COUNT;
+	}	
+	convert_to_long(bartlby_trap_id);
+	ZEND_FETCH_RESOURCE(bres, bartlby_res*, &zbartlby_resource, -1, BARTLBY_RES_NAME, le_bartlby);
+
+	
+	if (array_init(return_value) == FAILURE) {
+		RETURN_FALSE;
+	}
+	
+	shm_hdr=(struct shm_header *)(void *)bres->bartlby_address;
+	svcmap=(struct service *)(void *)(bres->bartlby_address+sizeof(struct shm_header));
+	wrkmap=(struct worker *)(void*)&svcmap[shm_hdr->svccount+1];
+	dtmap=(struct downtime *)(void*)&wrkmap[shm_hdr->wrkcount+1];
+	srvmap=(struct server *)(void*)&dtmap[shm_hdr->dtcount+1];
+	evntmap=(struct btl_event *)(void *)&srvmap[shm_hdr->srvcount+1];
+	srvgrpmap=(struct servergroup *)(void *)&evntmap[EVENT_QUEUE_MAX+1];
+	svcgrpmap=(struct servicegroup *)(void *)&srvgrpmap[shm_hdr->srvgroupcount+1];
+	trapmap=(struct trap *)(void *)&svcgrpmap[shm_hdr->svcgroupcount+1];
+	
+	if(Z_LVAL_P(bartlby_trap_id) > shm_hdr->trapcount-1) {
+		php_error(E_WARNING, "Trap id out of bounds");	
+		RETURN_FALSE;	
+	}
+
+	add_assoc_long(return_value, "trap_id", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_id);
+	add_assoc_string(return_value, "trap_name", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_name,1);
+	add_assoc_string(return_value, "trap_catcher", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_catcher,1);
+	add_assoc_string(return_value, "trap_status_text", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_status_text,1);
+	add_assoc_string(return_value, "trap_status_ok", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_status_ok,1);
+	add_assoc_string(return_value, "trap_status_warning", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_status_warning,1);
+	add_assoc_string(return_value, "trap_status_critical", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_status_critical,1);
+	add_assoc_long(return_value, "trap_service_id", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_service_id);
+	add_assoc_long(return_value, "trap_fixed_status", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_fixed_status);
+	add_assoc_long(return_value, "trap_prio", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_prio);
+	add_assoc_long(return_value, "trap_is_final", trapmap[Z_LVAL_P(bartlby_trap_id)].trap_is_final);
+	add_assoc_long(return_value, "orch_id", trapmap[Z_LVAL_P(bartlby_trap_id)].orch_id);
+
+}
+
+
+
+PHP_FUNCTION(bartlby_get_trap_by_id) {
+	zval * zbartlby_resource;
+	zval * trap_id;
+	
+	char * dlmsg;
+	int ret;
+	int (*GetTrapById)(int,struct trap *, char *);
+	struct trap  svc;
+	
+	bartlby_res * bres;
+
+	if (ZEND_NUM_ARGS() != 2 || zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rz", &zbartlby_resource,&trap_id)==FAILURE) {
+		WRONG_PARAM_COUNT;
+	}
+	
+	convert_to_long(trap_id);
+	ZEND_FETCH_RESOURCE(bres, bartlby_res*, &zbartlby_resource, -1, BARTLBY_RES_NAME, le_bartlby);
+	
+
+
+	LOAD_SYMBOL(GetTrapById,bres->SOHandle, "GetTrapById");
+	ret=GetTrapById(Z_LVAL_P(trap_id),&svc, bres->cfgfile);
+	
+	if(ret < 0) {
+		RETURN_FALSE;	
+	} else {
+		if (array_init(return_value) == FAILURE) {
+			RETURN_FALSE;
+		}
+			add_assoc_long(return_value, "trap_id", svc.trap_id);
+			add_assoc_string(return_value, "trap_name", svc.trap_name,1);
+			add_assoc_string(return_value, "trap_catcher", svc.trap_catcher,1);
+			add_assoc_string(return_value, "trap_status_text", svc.trap_status_text,1);
+			add_assoc_string(return_value, "trap_status_ok", svc.trap_status_ok,1);
+			add_assoc_string(return_value, "trap_status_warning", svc.trap_status_warning,1);
+			add_assoc_string(return_value, "trap_status_critical", svc.trap_status_critical,1);
+			add_assoc_long(return_value, "trap_service_id", svc.trap_service_id);
+			add_assoc_long(return_value, "trap_fixed_status", svc.trap_fixed_status);
+			add_assoc_long(return_value, "trap_prio", svc.trap_prio);
+			add_assoc_long(return_value, "trap_is_final", svc.trap_is_final);
+			add_assoc_long(return_value, "orch_id", svc.orch_id);
+	}
+		
+}
+
+
+PHP_FUNCTION(bartlby_add_trap) {
+	zval * zbartlby_resource;
+	zval * trap_name;
+	zval * trap_catcher;
+	zval * trap_status_text;
+	zval * trap_status_ok;
+	zval * trap_status_warning;
+	zval * trap_status_critical;
+	zval * trap_service_id;
+	zval * trap_fixed_status;
+	zval * trap_prio;
+	zval * trap_is_final;
+	zval * orch_id;
+	
+	
+	zval ** temp_pp;
+	zval * options_array;
+	
+	char * dlmsg;
+	
+	int ret;
+	
+	int (*AddTrap)(struct trap *,char *);
+	
+	struct trap svc;
+	bartlby_res * bres;
+	
+	if(ZEND_NUM_ARGS() != 2 || zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rz", &zbartlby_resource,&options_array) == FAILURE) {
+		WRONG_PARAM_COUNT;	
+	}
+	
+	if(Z_TYPE_P(options_array) != IS_ARRAY) {
+		php_error_docref(NULL TSRMLS_CC, E_ERROR, "SECOND parameter needs to be array object");
+		RETURN_BOOL(0);
+	}
+	
+	GETARRAY_EL_FROM_HASH(trap_name, "trap_name", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_catcher, "trap_catcher", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_status_text, "trap_status_text", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_status_ok, "trap_status_ok", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_status_warning, "trap_status_warning", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_status_critical, "trap_status_critical", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_service_id, "trap_service_id", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+	GETARRAY_EL_FROM_HASH(trap_fixed_status, "trap_fixed_status", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+	GETARRAY_EL_FROM_HASH(trap_prio, "trap_prio", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+	GETARRAY_EL_FROM_HASH(trap_is_final, "trap_is_final", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+	GETARRAY_EL_FROM_HASH(orch_id, "orch_id", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+
+
+
+	
+	ZEND_FETCH_RESOURCE(bres, bartlby_res*, &zbartlby_resource, -1, BARTLBY_RES_NAME, le_bartlby);
+	
+	convert_to_string(trap_name);
+	convert_to_string(trap_catcher);
+	convert_to_string(trap_status_text);
+	convert_to_string(trap_status_ok);
+	convert_to_string(trap_status_warning);
+	convert_to_string(trap_status_critical);
+	
+
+	convert_to_long(trap_service_id);
+	convert_to_long(trap_fixed_status);
+	convert_to_long(trap_prio);
+	convert_to_long(trap_is_final);
+	convert_to_long(orch_id);
+		
+	
+	
+	
+	LOAD_SYMBOL(AddTrap,bres->SOHandle, "AddTrap");
+	
+	strcpy(svc.trap_name, Z_STRVAL_P(trap_name));
+	strcpy(svc.trap_catcher, Z_STRVAL_P(trap_catcher));
+	strcpy(svc.trap_status_text, Z_STRVAL_P(trap_status_text));
+	strcpy(svc.trap_status_ok, Z_STRVAL_P(trap_status_ok));
+	strcpy(svc.trap_status_warning, Z_STRVAL_P(trap_status_warning));
+	strcpy(svc.trap_status_critical, Z_STRVAL_P(trap_status_critical));
+
+	svc.trap_service_id=Z_LVAL_P(trap_service_id);
+	svc.trap_fixed_status=Z_LVAL_P(trap_fixed_status);
+	svc.trap_prio=Z_LVAL_P(trap_prio);
+	svc.trap_is_final=Z_LVAL_P(trap_is_final);
+	svc.orch_id=Z_LVAL_P(orch_id);
+
+	
+	ret=AddTrap(&svc, bres->cfgfile);
+	BARTLBY_OBJECT_GONE(zbartlby_resource, bres,ret, BARTLBY_TRAP_GONE, BARTLBY_OBJECT_ADDED);
+	RETURN_LONG(ret);	
+}
+
+
+
+PHP_FUNCTION(bartlby_modify_trap) {
+	zval * zbartlby_resource;
+	zval * trap_name;
+	zval * trap_catcher;
+	zval * trap_status_text;
+	zval * trap_status_ok;
+	zval * trap_status_warning;
+	zval * trap_status_critical;
+	zval * trap_service_id;
+	zval * trap_fixed_status;
+	zval * trap_prio;
+	zval * trap_is_final;
+	zval * orch_id;	
+	zval * trap_id;
+
+
+
+	zval ** temp_pp;
+	zval * options_array;
+	char * dlmsg;
+	
+	int ret;
+	
+	int (*UpdateTrap)(struct trap *,char *);
+	
+	struct trap svc;
+	bartlby_res * bres;
+	
+	
+	
+	if(ZEND_NUM_ARGS() != 3 || zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rzz", &zbartlby_resource,&trap_id, &options_array) == FAILURE) {
+		WRONG_PARAM_COUNT;	
+	}
+	
+	if(Z_TYPE_P(options_array) != IS_ARRAY) {
+		php_error_docref(NULL TSRMLS_CC, E_ERROR, "SECOND parameter needs to be array object");
+		RETURN_BOOL(0);
+	}
+
+	GETARRAY_EL_FROM_HASH(trap_name, "trap_name", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_catcher, "trap_catcher", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_status_text, "trap_status_text", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_status_ok, "trap_status_ok", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_status_warning, "trap_status_warning", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_status_critical, "trap_status_critical", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_STRING, "default_name");
+	GETARRAY_EL_FROM_HASH(trap_service_id, "trap_service_id", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+	GETARRAY_EL_FROM_HASH(trap_fixed_status, "trap_fixed_status", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+	GETARRAY_EL_FROM_HASH(trap_prio, "trap_prio", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+	GETARRAY_EL_FROM_HASH(trap_is_final, "trap_is_final", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+	GETARRAY_EL_FROM_HASH(orch_id, "orch_id", temp_pp, options_array,BARTLBY_FIELD_REQUIRED,BARTLBY_DEFAULT_LONG, 1);
+
+	
+	
+	
+	ZEND_FETCH_RESOURCE(bres, bartlby_res*, &zbartlby_resource, -1, BARTLBY_RES_NAME, le_bartlby);
+	
+	convert_to_string(trap_name);
+	convert_to_string(trap_catcher);
+	convert_to_string(trap_status_text);
+	convert_to_string(trap_status_ok);
+	convert_to_string(trap_status_warning);
+	convert_to_string(trap_status_critical);
+	convert_to_long(trap_service_id);
+	convert_to_long(trap_fixed_status);
+	convert_to_long(trap_prio);
+	convert_to_long(trap_is_final);
+	convert_to_long(orch_id);
+		
+	
+	convert_to_long(trap_id);
+	
+	
+	
+	
+	LOAD_SYMBOL(UpdateTrap,bres->SOHandle, "UpdateTrap");
+
+	strcpy(svc.trap_name, Z_STRVAL_P(trap_name));
+	strcpy(svc.trap_catcher, Z_STRVAL_P(trap_catcher));
+	strcpy(svc.trap_status_text, Z_STRVAL_P(trap_status_text));
+	strcpy(svc.trap_status_ok, Z_STRVAL_P(trap_status_ok));
+	strcpy(svc.trap_status_warning, Z_STRVAL_P(trap_status_warning));
+	strcpy(svc.trap_status_critical, Z_STRVAL_P(trap_status_critical));
+
+	svc.trap_service_id=Z_LVAL_P(trap_service_id);
+	svc.trap_fixed_status=Z_LVAL_P(trap_fixed_status);
+	svc.trap_prio=Z_LVAL_P(trap_prio);
+	svc.trap_is_final=Z_LVAL_P(trap_is_final);
+	svc.orch_id=Z_LVAL_P(orch_id);	
+	svc.trap_id=Z_LVAL_P(trap_id);	
+	
+	BARTLBY_OBJECT_GONE(zbartlby_resource, bres,svc.trap_id, BARTLBY_TRAP_GONE, BARTLBY_OBJECT_CHANGED);
+	ret=UpdateTrap(&svc, bres->cfgfile);
+	
+	
+
+	RETURN_LONG(ret);		
+}
+
+
+PHP_FUNCTION(bartlby_delete_trap) {
+	zval * zbartlby_resource;
+	zval * trap_id;
+	
+	
+	char * dlmsg;
+	
+	int ret;
+	
+	int (*DeleteTrap)(int, char*);
+	
+	struct trap svc;
+	bartlby_res * bres;
+
+	
+	if (ZEND_NUM_ARGS() != 2 || zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rz", &zbartlby_resource,&trap_id)==FAILURE) {
+		WRONG_PARAM_COUNT;
+	}
+	ZEND_FETCH_RESOURCE(bres, bartlby_res*, &zbartlby_resource, -1, BARTLBY_RES_NAME, le_bartlby);
+	
+	
+	convert_to_long(trap_id);
+	
+	
+	
+	LOAD_SYMBOL(DeleteTrap,bres->SOHandle, "DeleteTrap");
+	
+	
+	BARTLBY_OBJECT_GONE(zbartlby_resource, bres,Z_LVAL_P(trap_id), BARTLBY_TRAP_GONE, BARTLBY_OBJECT_DELETED);
+	
+	ret=DeleteTrap(Z_LVAL_P(trap_id),bres->cfgfile);
+
+	
+
+	RETURN_LONG(ret);	
+}
+PHP_FUNCTION(bartlby_set_trap_id) {
+	zval * zbartlby_resource;
+	zval * from;
+	zval * to;
+	zval * mig;
+	
+	
+	char * dlmsg;
+	int ret;
+	int (*TrapChangeId)(int, int, char*);
+	bartlby_res * bres;
+	
+	
+	if (ZEND_NUM_ARGS() != 3 || zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "rzz", &zbartlby_resource,&from, &to)==FAILURE) {
+		WRONG_PARAM_COUNT;
+	}
+	ZEND_FETCH_RESOURCE(bres, bartlby_res*, &zbartlby_resource, -1, BARTLBY_RES_NAME, le_bartlby);
+	convert_to_long(from);
+	convert_to_long(to);
+	
+	
+	
+	LOAD_SYMBOL(TrapChangeId,bres->SOHandle, "TrapChangeId");
+	
+	
+	
+	ret=TrapChangeId(Z_LVAL_P(from),Z_LVAL_P(to),bres->cfgfile);
+	RETURN_LONG(ret);	
+}
+
+//TRAP
 
 
