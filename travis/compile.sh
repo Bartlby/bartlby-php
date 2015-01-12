15ebdbd417e89fbf6d92aefb019eb3637e117891
Helmut Januschka <helmut@januschka.com>
2015-01-12 08:45:52 +0000

    remove core build

--- travis/compile.sh
+++ travis/compile.sh
@@ -8,22 +8,7 @@ then
 	echo "compile failed";
 	exit $EX;
 fi;
-PHP_DIR=$PWD;
 
-
-
-CORE_DIR=$PWD;
-git clone https://github.com/Bartlby/bartlby-core.git bcore
-cd bcore
-travis/compile.sh
-cd $CORE_DIR;
-cp bcore/bartlby.cfg $PHP_DIR/
-
-
-cd $PHP_DIR;
-NO_INTERACTION=1 make test TESTS="-m"
-
-EX=$?;
 
 
 exit $EX;
