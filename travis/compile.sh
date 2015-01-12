fda2ac6efc2c1e952c951791e6cae05baece78e9
root <root@39d2c877bc4f>
2015-01-12 07:24:51 +0000

    unit test 0001

--- travis/compile.sh
+++ travis/compile.sh
@@ -2,13 +2,27 @@ phpize
 
 ./configure
 make
-if [ $? != 0 ]; then
-	exit $?;
+EX=$?;
+if [ $EX != 0 ];
+then
+	echo "compile failed";
+	exit $EX;
 fi;
-make install
-ls
+PHP_DIR=$PWD;
 
-exit 0;
+
+
+git clone https://github.com/Bartlby/bartlby-core.git bcore
+cd bcore
+travis/compile.sh
+
+cd $PHP_DIR;
+NO_INTERACTION=1 make test TESTS="-m"
+
+EX=$?;
+
+
+exit $EX;
 
 
 
