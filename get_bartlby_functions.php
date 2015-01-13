562f1dea6c0b9cc394ff4f766c6543f12ee9114c
Helmut Januschka <helmut@januschka.com>
2015-01-13 08:21:55 +0000

    tests

--- get_bartlby_functions.php
+++ get_bartlby_functions.php
@@ -1,6 +1,6 @@
 <?
 $f = get_defined_functions();
-$f=$f[internal];
+$f=$f["internal"];
 for($x=0; $x<count($f); $x++) {
 	if(preg_match("/bartlby_/i", $f[$x])) {
 		echo $f[$x] . "\n";
--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 NO_INTERACTION=1 TEST_PHP_ARGS="-m --show-diff"  make test
 
+#check for untested functions
+php get_bartlby_functions.php | while read a; do o=$(grep $a tests/*.phpt); EX=$?; if [ $EX != 0 ]; then echo "$a not tested"; fi;  done
+
+
