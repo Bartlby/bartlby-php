7b112de2cc3d47ad72998fa1f2470e3dfa096536
Helmut Januschka <helmut@januschka.com>
2015-02-09 06:26:27 +0000

    php7

--- travis/compile.sh
+++ travis/compile.sh
@@ -1,5 +1,32 @@
-phpize
+if [ "$TRAVIS_BRANCH" = "php7" ];
+then
+	echo "Installing PHP7";
+	mkdir -p $HOME/php
+	git clone --depth 1 https://github.com/php/php-src $HOME/php/src
 
+	cd $HOME/php/src
+	./buildconf --force
+	./configure --prefix=$HOME --disable-all --enable-maintainer-zts
+	make -j2 --quiet install
+
+	cd $TRAVIS_BUILD_DIR
+	$HOME/bin/phpize
+	./configure --with-php-config=$HOME/bin/php-config 
+	make -j2 --quiet
+	EX=$?;
+	if [ $EX != 0 ];
+	then
+		echo "compile failed";
+		exit $EX;
+	fi;
+
+
+
+	exit $EX;
+fi;
+
+
+phpize
 ./configure 
 make
 EX=$?;
